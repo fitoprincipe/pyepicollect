8f34db904c338cf478b0dc5f46d1cca3bd770c9e
Rodrigo E. Principe <fitoprincipe82@gmail.com>
2019-02-13 09:28:05 -0300

    initial commit

--- pyepicollect/__init__.py
+++ pyepicollect/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 """ Python API for reading EpiCollect 5 data (https://five.epicollect.net/) """
-import requests
 
-
-__version__ = 5.0.0
+__version__ = '5.0.0'
---
+++ pyepicollect/auth.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+""" Authentication module for EpiCollect 5 """
+import requests
+from datetime import datetime
+
+TOKEN_REQ_URL = 'https://five.epicollect.net/api/oauth/token'
+REQ_URL = 'https://five.epicollect.net/api'
+
+
+def request_token(client_id, client_secret):
+    """ Get token. Each token is valid for 2 hours
+
+    :return: A dictionary with the following keys:
+
+    :rtype: dict
+    """
+
+    token_data = requests.post(TOKEN_REQ_URL, data={
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    })
+
+    time_now = datetime.now()
+    token_data['request_time'] = time_now
+
+    return token_data
+
+
+
